122b8044d97b7e2c7d37b3f068aae5af1dd4dde2
Chema Peribáñez <chema@softlibre.net>
2015-05-19 15:03:07 +0200

    put all ami related code in a file

--- glancesync_ami.py
+++ glancesync_ami.py
@@ -1,1 +1,143 @@
-__author__ = 'jomar'
+#!/usr/bin/env python
+# -- encoding: utf-8 --
+#
+# Copyright 2015 Telefónica Investigación y Desarrollo, S.A.U
+#
+# This file is part of FI-Core project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For those usages not covered by the Apache version 2.0 License please
+# contact with opensource@tid.es
+#
+author = 'jmpr22'
+
+import logging
+"""This internal module check and update the kernel_id and ramdisk_id of
+AMI images. This metadata points to the UUID of two auxiliary images: the
+kernel and the ramdisk.
+
+The images has the same name on all regions, but the UUIDs are different. To
+solve this, first in the dictionary with the images of the master region, the
+ramdisk_id and kernel_id UUID values are replaced with the names. Then, the
+code of this module is called for each AMI image on the other regions, to
+locate the auxiliary images on the region and update the kernel_id and
+ramdisk_id.
+"""
+
+
+def get_master_region_dict(image_list):
+    """Convert the image list to a dictionary indexed by name and also
+    replace at parameters kernel_id and ramdisk_id the UUID with the name of
+    the image.
+
+    :param image_list: the list of images on the master region
+    :return: a dictionary indexed by name
+    """
+
+    master_region_dictimagesbyid = dict(
+        (image.id, image) for image in image_list if image.status == 'active')
+    master_region_dictimages = dict()
+    for image in image_list:
+        if 'kernel_id' in image.user_properties:
+            image.user_properties['kernel_id'] = master_region_dictimagesbyid[
+                image.user_properties['kernel_id']].name
+
+        if 'ramdisk_id' in image.user_properties:
+            image.user_properties['ramdisk_id'] = master_region_dictimagesbyid[
+                image.user_properties['ramdisk_id']].name
+
+        master_region_dictimages[image.name] = image
+    return master_region_dictimages
+
+
+def update_kernelramdisk_id(image, master_images, region_images,
+                            pending_kernel, pending_ramdisk):
+    """Modify the kernel_id and ramdisk_id if required of an AMI image.
+
+    This function modify the object in memory, but not in the glance server.
+    The function returns True if the object has been modified and need to be
+    updated.
+
+    Dictionaries pending_kernel and pending_ramdisk are updated if any kernel
+    or ramdisk image is missing. The key is the name of the image and the
+    value the UUID of the image whose ramdisk_id/kernel_id property must be
+    updated.
+
+    :param image: the image to modify
+    :param master_images: the master image with the same name. In this image
+    kernel_id and ramdisk_id have a name instead of an UUID, while kernel_id
+     and ramdisk_id are the UUID of the images in the same region than image.
+    :param region_images: a dictionary with the images of the region, indexed
+      by name.
+    :return: True if the image was modified, False otherwise.
+    """
+
+    result_
+    if not _update_auximg_id('kernel_id', image, master_images, region_images):
+        return False
+    if not _update_auximg_id('ramddisk_id', image, master_images,
+                             region_images):
+        return False
+    return True
+
+
+def _update_auximg_id(property_id, image, master_image, images_region):
+    """
+    Utility function that modify, if required, a property in the image
+    pointing to the id of other image. For example, it is used with kernel_id
+    and ramdisk_id of AMI images.
+
+    param property_id: the name of the property (e.g. kernel_id, ramdisk_id)
+    :param image: the image to modify
+    :param master_image: the master image
+    :param images_region:
+    :return:
+    """
+    if property_id not in master_image.user_properties:
+        if property_id not in image.user_properties:
+            # Nothing to do
+            return False
+        else:
+            # remove property
+            del image.user_properties[property_id]
+            return True
+
+    # Get the name of the kernel/ramdisk image
+    aux_image_name = master_image.user_properties[property_id]
+
+    if aux_image_name not in images_region:
+        # It is very unusual that an image exists and not its kernel or
+        # ramdisk, because little images are upload first, but it is
+        # possible (e.g. if the image is removed manually or it is
+        # replaced by a more recent one)
+        # In this case, print a warning and mark the image as private
+        image.is_public = 'No'
+        msg = 'Not found {0} on region {1}. It should be {2} of image {3}'
+        logging.warning(msg.format(aux_image_name, image.region,
+                                   property_id, image.name))
+        # Put the aux image name; this provides information to the caller
+        # about the missing image
+        image.user_properites[property_id] = aux_image_name
+        return True
+
+    aux_image = images_region[aux_image_name]
+    if property_id in image.user_properties and\
+            aux_image.id == image.user_properties[property_id]:
+        return False
+
+    image.user_properties[property_id] = aux_image.id
+    return True
