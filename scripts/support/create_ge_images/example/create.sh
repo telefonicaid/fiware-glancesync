4bd68615d415043fc28af23e0bb9980236aa099b
Chema Peribáñez <chema@softlibre.net>
2015-09-28 12:54:31 +0200

    add an example

---
+++ scripts/support/create_ge_images/example/create.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright 2015 Telefónica Investigación y Desarrollo, S.A.U
+#
+# This file is part of FI-Core project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For those usages not covered by the Apache version 2.0 License please
+# contact with opensource@tid.es
+#
+
+# As a guideline to do an unattended installation, see http://www.microhowto.info/howto/perform_an_unattended_installation_of_a_debian_package.html
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update -q
+sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  apache2-mpm-prefork 
+sudo tar xzf data.tgz -C/
+rm data.tgz
+sudo tee <EOF /etc/apache2/conf.d/myconf.conf >/dev/null
+# Add here your own configuraton changes
+
+EOF
+
+# remember to use sudo!!!
