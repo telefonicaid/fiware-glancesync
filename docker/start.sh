eede3ddbe0a065e98cd21f12b94fcff69fb7aa87
visitor <henar.munozfrutos@telefonica.com>
2016-04-04 17:27:45 +0200

    update

--- docker/start.sh
+++ docker/start.sh
@@ -9,9 +9,6 @@ sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" /etc/fiware.d/etc/fiware-glancesync.
 sed -i -e "s/{ADM_TENANT_ID}/${ADM_TENANT_ID}/" /etc/fiware.d/etc/fiware-glancesync.cfg
 sed -i -e "s/{ADM_USERNAME}/${ADM_USERNAME}/" /etc/fiware.d/etc/fiware-glancesync.cfg
 
-pip install virtualenv
-virtualenv venv
-. venv/bin/activate
 pip install -r requirements.txt
 export PYTHONPATH=../..
   
@@ -22,4 +19,3 @@ behave features/scripts/ --tags ~@skip --junit --junit-directory testreport
 
 # Execute Behave features of API:
 behave features/api/ --tags ~@skip --junit --junit-directory testreport
-sleep 12000
