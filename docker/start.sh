6e10fd2754bf04ab8583ad87866587e0ffb48896
visitor <henar.munozfrutos@telefonica.com>
2016-04-11 13:35:12 +0200

    update for including two keystone 3 glances

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,7 @@
 sed -i -e "s/{ADM_TENANT_NAME}/${ADM_TENANT_NAME}/" conf/settings.json
 sed -i -e "s/{ADM_PASSWORD}/${ADM_PASSWORD}/" conf/settings.json
 sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" conf/settings.json
+sed -i -e "s/{KEYSTONE_IP2}/${KEYSTONE_IP2}/" conf/settings.json
 sed -i -e "s/{ADM_TENANT_ID}/${ADM_TENANT_ID}/" conf/settings.json
 sed -i -e "s/{ADM_USERNAME}/${ADM_USERNAME}/" conf/settings.json
 sed -i -e "s/{ADM_TENANT_NAME}/${ADM_TENANT_NAME}/" /etc/fiware.d/etc/fiware-glancesync.cfg
