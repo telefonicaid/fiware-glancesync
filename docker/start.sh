20167a26c92d74631d74c1eb7b248f393a881faa
visitor <henar.munozfrutos@telefonica.com>
2016-04-12 13:53:58 +0200

    update the configuraiton for glancesync

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,23 @@
+export OS_REGION_NAME=$Region1
+export  OS_USERNAME=$ADM_USERNAME
+export OS_PASSWORD=$ADM_PASSWORD
+export OS_TENANT_NAME=$ADM_TENANT_NAME
+export  OS_AUTH_URL=http://$KEYSTONE_IP:5000/v3
+export OS_AUTH_URL_V2=http://$KEYSTONE_IP:5000/v2.0/
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_NAME=Default
+export OS_IDENTITY_API_VERSION=3
+
+openstack project show qa > qa
+
+export TENANTID_QA=`grep "| id" qa | awk 'NR==1{print $4}'`
 sed -i -e "s/{ADM_TENANT_NAME}/${ADM_TENANT_NAME}/" conf/settings.json
 sed -i -e "s/{ADM_PASSWORD}/${ADM_PASSWORD}/" conf/settings.json
 sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" conf/settings.json
 sed -i -e "s/{KEYSTONE_IP2}/${KEYSTONE_IP2}/" conf/settings.json
 sed -i -e "s/{ADM_TENANT_ID}/${ADM_TENANT_ID}/" conf/settings.json
 sed -i -e "s/{ADM_USERNAME}/${ADM_USERNAME}/" conf/settings.json
+sed -i -e "s/{TENANT_ID_QA}/${TENANT_ID_QA}/" conf/settings.json
 sed -i -e "s/{ADM_TENANT_NAME}/${ADM_TENANT_NAME}/" /etc/fiware.d/etc/fiware-glancesync.cfg
 sed -i -e "s/{ADM_PASSWORD}/${ADM_PASSWORD}/" /etc/fiware.d/etc/fiware-glancesync.cfg
 sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" /etc/fiware.d/etc/fiware-glancesync.cfg
@@ -14,12 +28,10 @@ sed -i -e "s/{Region1}/${Region1}/" conf/settings.json
 sed -i -e "s/{Region2}/${Region2}/" conf/settings.json
 sed -i -e "s/{Region3}/${Region3}/" conf/settings.json
 
-export OS_REGION_NAME=$Region1
-export  OS_USERNAME=$ADM_USERNAME
-export OS_PASSWORD=$ADM_PASSWORD
-export OS_TENANT_NAME=$ADM_TENANT_NAME
-export  OS_AUTH_URL=http://$KEYSTONE_IP:5000/v2.0
+
+
 export PYTHONPATH=../..
+sleep 1200000000000
 behave features/glancesync/ --tags ~@skip --junit --junit-directory testreport
 
 # Execute Behave features of script components:
